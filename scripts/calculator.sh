65846be1e4009ebfa852d93653579724b68e18e0
Navaganesh Raju <navaganeshr69@gmail.com>
2022-11-19 17:28:19 +0530

    adding junk

--- scripts/calculator.sh
+++ scripts/calculator.sh
@@ -6,4 +6,6 @@ NUMBER2=$2
 echo "Number1: ${NUMBER1}"
 echo "Number2: ${NUMBER2}"
 SUM=`expr $NUMBER1 + $NUMBER2`
+echo "############################"
 echo "Sum of 2 numbers is : ${SUM}"
+echo "############################"
